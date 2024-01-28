a7528da5da3d69eef9eb37829abba7fcd02a2242
Paul Schmidt <pauldschmidtofficial@gmail.com>
2024-01-28 01:56:59 -0800

    dumpy menu

--- mainMenu.js
+++ mainMenu.js
@@ -1,24 +1,20 @@
 class MainMenu {
     constructor() {
-        
-        
-        this.menu = document.createElement("canvas");
-        this.menu.id = "menu";
-        this.menu.width = 1024;
-        this.menu.height = 522;
-        this.menu.tabIndex = "0";
-        this.menu.style.background = "violet";
-        this.menu.style.border = "1px solid";
-        document.body.appendChild(this.menu);
-        this.ctx = menu.getContext("2d");
-        this.menu.addEventListener("click", e => {
+        // this.menu = document.createElement("div");
+        // this.menu.id = "mainMenu";
+        this.menu = document.getElementById("mainMenu");
+        // this.menu.tabIndex = 0;
+        // document.body.appendChild(this.menu);
+        // this.ctx = menu.getContext("2d");
+        this.startButton = document.getElementById("startRaceButton");
+        this.startButton.addEventListener("click", e => {
 
             console.log("CLICK");
             sceneManager.loadRace();
             this.hideMenu();
             
         });
-        this.ctx.drawImage(ASSET_MANAGER.getAsset("Sprites/Menu/fzero_title.png"), 0, 0);
+        // this.ctx.drawImage(ASSET_MANAGER.getAsset("Sprites/Menu/fzero_title.png"), 0, 0);
         this.showMenu();
     }
     hideMenu() {
